95e2ed2eb153f212afc0a4658075806693d3d7b3
ketanmaheshwari1502 <ketanmaheshwari1502@gmail.com>
2025-06-12 16:33:04 +0530

    mergeinterval

---
+++ Week1_Basics/56_MergeInterval.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        if (intervals.empty()) return res;
+
+        sort(intervals.begin(), intervals.end());
+
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                // Merge overlapping intervals
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;
+    }
+};
